0114c07303f1805546a606dcdff4d98a5cbe8c68
ManhTien <manhtien4study@gmail.com>
2024-12-13 19:12:24 +0700

    fix port

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 const hostname = process.env.HOST_NAME;
 
 
